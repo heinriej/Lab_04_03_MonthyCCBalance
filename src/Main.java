085312c142c241152ac3f3a51bb6f0afc795d9d3
heinriej <heinirej@mail.uc.edu>
2024-09-23 11:54:58 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        double creditCardBal = 5000;
+        double balance = 0;
+        double interest = 0;
+        final double INTEREST_RATE = 0.17;
+
+        interest = creditCardBal * INTEREST_RATE;
+        balance = creditCardBal + interest;
+        System.out.println("After one month, the credit card balance of " + creditCardBal + " will be " + balance);
+
+        creditCardBal = balance;
+        interest = creditCardBal * INTEREST_RATE;
+        balance = creditCardBal + interest;
+        System.out.println("After two months, the credit card balance of " + creditCardBal + " will be " + balance);
+
     }
 }
